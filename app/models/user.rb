8fa84bbf134fefeaa846fd5bf5a94120999927ed
giangnguyen95 <giangnt_58@vnu.edu.vn>
2016-04-03 13:24:19 +0000

    resize gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   include Gravtastic 
-  gravtastic
+  gravtastic :secure => true,
+              :filetype => :gif,
+              :size => 30
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
