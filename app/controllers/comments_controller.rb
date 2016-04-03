4fecf432a58ec9e059e863deccb64e22c49a2c95
giangnguyen95 <giangnt_58@vnu.edu.vn>
2016-04-03 04:50:16 +0000

    tmp

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
+        
         format.html { redirect_to idea_path(@comment.idea_id), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
